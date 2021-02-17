9500b75a867f795591174cd7b6578bce2516d48a
Carlos Leite <CarlosLeite@criticalmanufacturing.com>
2021-02-17 15:28:13 +0000

    changes

--- GUI/MathMaka.cs
+++ GUI/MathMaka.cs
@@ -8,6 +8,11 @@ namespace GUI
 {
     public class MathMaka
     {
-
+        public void MakeOperations()
+        {
+            Lib1.Lib1Math.Minus(1, 1);
+            Lib2.Lib2Math.Multiply(1, 1);
+            Lib3.Lib3Math.Plus(1, 1);
+        }
     }
 }
